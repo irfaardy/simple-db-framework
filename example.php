2a35f42e79fcf203148d0748cdb4b877270d3370
IRFA <irfa.backend@protonmail.com>
2020-03-27 14:29:40 +0700

    Update example.php

--- example.php
+++ example.php
@@ -5,4 +5,5 @@ use Irfa\DBOperation as DB;
  $res = DB::table('book')->get();
      foreach ($res as $r):
         echo $r['title']."<br>";
+        echo $r['description']."<br>";
       endforeach;
